7eab7d730e9c178d7e94d3d95b4cab2aa4246058
mhewson <mhewson@gmail.com>
2018-03-29 16:19:47 -0700

    adding support for rendered example using boxshadow

--- src/components/TypeInset.vue
+++ src/components/TypeInset.vue
@@ -3,7 +3,7 @@
     <td>{{prop.name}}</td>
     <td>{{prop.value}}</td>
     <td>
-      <div class="inset-example" :style="{boxShadow: `${prop.value}`}"/>
+      <div class="inset-example" :style="{boxShadow: inset, padding: pad}">{{ prop.name }}</div>
     </td>
   </tr>
 </template>
@@ -14,14 +14,39 @@ export default {
   props: {
     prop: Object,
   },
+  computed: {
+    pad() {
+      const val = this.prop.value;
+      if (val.indexOf(' ') <= 0) {
+        return `${val} ${val}`;
+      } else if (val.indexOf('*') > 0) {
+        let [x, y] = val.split(') '); // eslint-disable-line
+        return `${x}) ${y}`;
+      }
+      const [x, y] = val.split(' ');
+      return `${x} ${y}`;
+    },
+    inset() {
+      const val = this.prop.value;
+      if (val.indexOf(' ') <= 0) {
+        return this.getInset(val, val, `-${val}`, `-${val}`);
+      } else if (val.indexOf('*') > 0) {
+        let [x, y] = val.split(') '); // eslint-disable-line
+        const negx = `${x.slice(0, 5)}-${x.slice(5)})`;
+        return this.getInset(`${x})`, y, negx, `-${y}`);
+      }
+      const [x, y] = val.split(' ');
+      return this.getInset(x, y, `-${x}`, `-${y}`);
+    },
+  },
+  methods: {
+    getInset(posy, posx, negy, negx) {
+      console.log(posx, posy, negx, negy);
+      return `inset ${negx} ${negy} 0 rgb(199, 220, 191), inset ${posx} ${posy} 0 rgb(199, 220, 191)`;
+    },
+  },
 };
 </script>
 
 <style>
-  .inset-example {
-    border-color: black;
-    border-style: solid;
-    width: 350px;
-    height: 200px;
-  }
 </style>
