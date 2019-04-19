0f19a49568e2400bd0f1d3890f5f7bd9c07590b4
Lance Harper <laharpe@rei.com>
2019-04-19 13:13:04 -0700

    Update breakpoint units

--- style-dictionary/transforms/size/strip-px.js
+++ style-dictionary/transforms/size/strip-px.js
@@ -4,6 +4,9 @@ module.exports = (StyleDictionary) => {
   StyleDictionary.registerTransform({
     name: 'size/strip-px',
     type: 'value',
+    matcher(prop) {
+      return prop.attributes.category !== 'breakpoint';
+    },
     transformer(prop) {
       let cleanVal = prop.value;
       if (_.endsWith(prop.value, 'px')) {
