ffda361f38a8f5fa728ebeb08443d603894950ee
Cole Willsea <cowills@rei.com>
2020-04-02 11:10:51 -0700

    create strip-all-px transform for js, exclude prominence from it

--- style-dictionary/transformGroups/js.js
+++ style-dictionary/transformGroups/js.js
@@ -6,7 +6,7 @@ module.exports = (StyleDictionary) => {
       'attribute/deprecated',
       'attribute/cdr-cti',
       'name/cti/pascal',
-      'size/strip-all-px',
+      'size/strip-all-px-js',
       'color/css',
     ],
   });
