8b8f845e10f6b150c9feefc18a9c74818147dd3e
Santiago Agudelo <saagude@rei.com>
2026-03-31 12:15:37 -0500

    refactor(duration tokens): added motion files for duration and timing

--- dist/rei-dot-com/js/foundations/cdr-motion.cjs
+++ dist/rei-dot-com/js/foundations/cdr-motion.cjs
@@ -5,7 +5,7 @@ module.exports = {
   CdrDuration4X: "400ms",
   CdrDuration5X: "500ms",
   CdrDuration6X: "600ms",
-  CdrDurationTimingFunctionEaseOut: "cubic-bezier(0.32, 0.94, 0.60, 1)",
-  CdrDurationTimingFunctionEase: "cubic-bezier(0.15, 0, 0.15, 0)",
-  CdrDurationTimingFunctionLinear: "cubic-bezier(0, 0, 1, 1)",
+  CdrTimingFunctionEaseOut: "cubic-bezier(0.32, 0.94, 0.60, 1)",
+  CdrTimingFunctionEase: "cubic-bezier(0.15, 0, 0.15, 0)",
+  CdrTimingFunctionLinear: "cubic-bezier(0, 0, 1, 1)",
 };
